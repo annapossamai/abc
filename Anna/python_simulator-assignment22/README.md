e615c126dfc32d08fd398de7f0714751a0f89c93
annapossamai <114078218+annapossamai@users.noreply.github.com>
2023-05-25 14:50:19 +0000

    Add files via upload

---
+++ Anna/python_simulator-assignment22/README.md
@@ -0,0 +1,106 @@
+
+# *PYTHON ROBOT SIMULATOR*
+
+> Professor Carmine Recchiuto, <p>Student: Anna Possamai
+
+
+## *RUNNING THE CODE*
+The simulator requires a Python 2.7 installation, the pygame library, PyPyBox2D, and PyYAML.
+
+You can run this project by write in the terminal this command:
+```
+$ python run.py Assignment.py
+```
+
+## *ASSIGNMENT GOAL*
+
+The goal of this assigment is to search and find a silver token and bring it near to a golden token so that in the final there will be golden and silver tokens distribuited in pairs.
+
+## *ROBOT*
+
+
+### *Robot API*
+
+
+The API for controlling a simulated robot is designed to be as similar as possible to the [SR API][sr-api].
+
+### *Motors* ###
+
+The simulated robot has two motors configured for skid steering, connected to a two-output [Motor Board](https://studentrobotics.org/docs/kit/motor_board). The left motor is connected to output `0` and the right motor to output `1`.
+
+The Motor Board API is identical to [that of the SR API](https://studentrobotics.org/docs/programming/sr/motors/), except that motor boards cannot be addressed by serial number. So, to turn on the spot at one quarter of full power, one might write the following:
+
+```python
+R.motors[0].m0.power = 25
+R.motors[0].m1.power = -25
+```
+
+### *The Grabber* ###
+
+The robot is equipped with a grabber, capable of picking up a token which is in front of the robot and within 0.4 metres of the robot's centre. To pick up a token, call the `R.grab` method:
+
+```python
+success = R.grab()
+```
+
+The `R.grab` function returns `True` if a token was successfully picked up, or `False` otherwise. If the robot is already holding a token, it will throw an `AlreadyHoldingSomethingException`.
+
+
+To drop the token, call the `R.release` method.
+
+Cable-tie flails are not implemented.
+
+### *Vision* ###
+
+To help the robot find tokens and navigate, each token has markers stuck to it, as does each wall. The `R.see` method returns a list of all the markers the robot can see, as `Marker` objects. The robot can only see markers which it is facing towards.
+
+Each `Marker` object has the following attributes:
+
+* `info`: a `MarkerInfo` object describing the marker itself. Has the following attributes:
+  * `code`: the numeric code of the marker.
+  * `marker_type`: the type of object the marker is attached to (either `MARKER_TOKEN_GOLD`, `MARKER_TOKEN_SILVER` or `MARKER_ARENA`).
+  * `offset`: offset of the numeric code of the marker from the lowest numbered marker of its type. For example, token number 3 has the code 43, but offset 3.
+  * `size`: the size that the marker would be in the real game, for compatibility with the SR API.
+* `centre`: the location of the marker in polar coordinates, as a `PolarCoord` object. Has the following attributes:
+  * `length`: the distance from the centre of the robot to the object (in metres).
+  * `rot_y`: rotation about the Y axis in degrees.
+* `dist`: an alias for `centre.length`
+* `res`: the value of the `res` parameter of `R.see`, for compatibility with the SR API.
+* `rot_y`: an alias for `centre.rot_y`
+* `timestamp`: the time at which the marker was seen (when `R.see` was called).
+
+## *CODE* ##
+
+### *FLOWCHART* ###
+![Flowchart](FinalFlowchart.png )
+
+
+At the beginning the program looks for (with `R.see()` function) the nearest silver token with the function:
+```python    
+dist, rot_y, codeS = find_silver_token()
+```
+Once it has checked that the `codeS` of the silver token is not in `listS`, the program approaches the found silver token with the `turn()` and `drive()` functions.
+
+When the robot is close to the silver token, it grabs it (via the `R.grab()` command) and goes to the nearest golden token (via the `GoToGolden()` function).
+
+The `GoToGolden` function works like this:
+1. First it searches for the nearest golden token;
+2. Then, once it has been checked that the `codeG` is not in the `listG`, the robot approaches the found golden token in the same way used to approach the silver token;
+3. Finally, when the robot is close to the golden token, it releases the silver token with the function `R.Release()`, then registers the golden token in listG, and so it moves away.
+
+These functions are iterated within the `while()` loops in the program until the robot sees all the blocks paired once it has turned at least 360° (this condition is improved by a counter (`cont`)).
+
+Finally, when the counter reaches the set threshold, the program ends.
+
+## *FUTURE IMPROVEMENTS*
+
+In some positions, when blocks are in the middle of the robot's 'moving area, it could drag these tokens with it, creating **collisions**.
+
+To avoid this, special functions could be created to increasing its "**environmental awareness**".
+
+In addition, when the robot searches for the nearest block, if the block is close but behind the robot itself, the robot cannot see it. If, on the other hand, the robot had **360-degree vision**, then it would be able to see it and catch it, making the program more efficient.
+
+Another possible improvement could be to create a **map of the arena**. If the robot could record the positions of the various tokens in the arena by creating a map of the arena itself, it would be much faster because it would no longer be necessary to scan the arena within each while cycle. 
+This would also allow a better **path management**, e.g. avoiding obstacles (represented in this case by the tokens themselves).
+ 
+Therefore, in order to achieve these improvements, as well as appropriate functions, it could be necessary to **increase the robot's vision range**.
---
+++ Anna/python_simulator-assignment22/robot-sim/Assignment.py
@@ -0,0 +1,184 @@
+from __future__ import print_function
+
+import time
+from sr.robot import *
+
+"""
+run with:
+    python2 run.py assigment.py
+"""
+
+# Inizio del timer
+start_time = time.time()
+
+R = Robot()
+""" instance of the class Robot"""
+
+a_th = 2.0
+""" float: Threshold for the control of the linear distance"""
+
+d_th = 0.4
+""" float: Threshold for the control of the orientation"""
+
+silver = True
+""" boolean: variable for letting the robot know if it has to look for a silver or for a golden marker"""
+
+
+def drive(speed, seconds):
+    """
+    Function for setting a linear velocity
+    
+    Args:   speed (int): the speed of the wheels
+            seconds (int): the time interval
+    """
+    R.motors[0].m0.power = speed
+    R.motors[0].m1.power = speed
+    time.sleep(seconds)
+    R.motors[0].m0.power = 0
+    R.motors[0].m1.power = 0
+
+def turn(speed, seconds):
+    """
+    Function for setting an angular velocity
+    
+    Args:   speed (int): the speed of the wheels
+            seconds (int): the time interval
+    """
+    R.motors[0].m0.power = speed
+    R.motors[0].m1.power = -speed
+    time.sleep(seconds)
+    R.motors[0].m0.power = 0
+    R.motors[0].m1.power = 0
+
+def find_silver_token():
+    """
+    Function to find the closest silver token
+
+    Returns:
+        dist (float): distance of the closest silver token (-1 if no silver token is detected)
+        rot_y (float): angle between the robot and the silver token (-1 if no silver token is detected)
+    """
+
+    dist=100
+    for token in R.see():
+        if token.dist < dist and token.info.marker_type is MARKER_TOKEN_SILVER:
+            dist=token.dist
+            rot_y=token.rot_y
+            codeS=token.info.code
+    if dist==100:
+        return -1, -1, -1
+    else:
+           return dist, rot_y, codeS
+
+
+def find_golden_token():
+    """
+    Function to find the closest golden token
+
+    Returns:
+    dist (float): distance of the closest golden token (-1 if no golden token is detected)
+    rot_y (float): angle between the robot and the golden token (-1 if no golden token is detected)
+    """
+    distG=100
+    for token in R.see():
+        if token.dist < distG and token.info.marker_type is MARKER_TOKEN_GOLD:
+            distG=token.dist
+            rotG_y=token.rot_y 
+            codeG=token.info.code
+    if distG==100:
+        return -1, -1, -1
+    else:
+        return distG, rotG_y, codeG
+
+def GoToGolden():
+    """
+    Function to check the presence of the closest golden token and to release the silver token near to it.
+    """
+    a=1 # constant to end the while loop
+    while a==1:
+        distG, rotG_y, codeG= find_golden_token() # the robot looks for a golden token
+        if (codeG==-1):
+            turn(20,0.5)
+        elif(codeG not in listG and codeG !=-1): # Control to avoid bringing golden token already paired
+            print("the Golden code is:",codeG)
+            b=1 # constant to end the while loop
+            while (distG>=d_th and b is 1):
+                distG, rotG_y, codeG= find_golden_token()
+                if distG==-1: # if no token is detected, the robot will turn 
+                    print("I don't see any golden token!")
+                    turn(+20,1)
+                if distG<(d_th+0.4): # if the robot closes to the golden token, it releases the silver token in that position and registers the codeG within listG
+                    print("Here we are")            
+                    R.release()
+                    # print("iniziale golden: ", listG)
+                    listG.append(codeG)
+                    # print("finale golden: ", listG)
+                    print("Token released!")
+                    b=2     # end the second while loop
+                    a=2     # end the first while loop
+                elif-a_th<=rotG_y<=a_th: # if the robot is well aligned with the token, it goes forward
+                    print("Ah, I'll go on a bit")
+                    drive(100,0.2)
+                elif rotG_y<-a_th: # if the robot is not well aligned with the token, it will turn on the left or on the right
+                    print("Turn left a bit")
+                    turn(-2,0.5)
+                elif rotG_y> a_th:
+                    print("Turn right a bit")
+                    turn(2,0.5)
+        else:   # if codeG is in listG the robot will turn a bit
+            print("The golden token was already included in the listG!")
+            turn(2,0.5)         
+
+
+cont=0      # Counter for ending the program (it counts how many turns the robot does without see anything)
+listS=[]    # registers the silver token grabbed by the robot
+listG=[]    # registers the golden token near the silver token released.
+
+file = open("/root/Desktop/Anna/tempo.txt", "a")
+while 1:
+    if (cont==4):  # 12 is the number of steps required to reach a more than 360 degrees angle (at the setted (speed,seconds)=(20,0.5)). 
+                    # This allows to have at least a 360 degrees view of the arena.     
+        print("END")
+        # Fine del timer
+	end_time = time.time()
+
+	# Calcolo del tempo di esecuzione in secondi
+	execution_time = end_time - start_time
+
+	# Salvataggio del tempo di esecuzione in un file di testo
+	
+	file.write("OFFSET 1: "+ str(execution_time)+ "\n")
+        exit()
+        
+    dist, rot_y, codeS= find_silver_token()    # the robot find the closest silver token 
+    if dist==-1:        # if the robot can't see any silver token (`dist=-1`) then it will turn and the counter will increase its value.
+        print("I don't see any silver token!")
+        turn(30,0.5)
+        cont=cont+1
+    elif codeS not in listS:  # control to avoid bringing silver token already paired. 
+        # The following "if" are steps to check the proximity of the found silver token.
+        if dist<d_th: # when the robot is close to the silver token, it grabs it and goes to the nearest golden token (via GoToGolden()) and then it moves away
+            print("I found a silver token!")
+            cont=0
+            if R.grab():
+                print("Gotcha!")
+                listS.append(codeS)
+                GoToGolden()
+                drive(-20,1)
+                turn(30,1)
+        elif -a_th<=rot_y<=a_th: # if the robot is well aligned with the token, it goes forward
+            print("Ah, that I'll do")
+            drive(80,0.2)
+        elif rot_y<=-a_th: # if the robot is not well aligned with the token, it will turn on the left or on the right
+            print("Left a bit")
+            turn(-2,0.5)
+        elif rot_y>= a_th:
+            print("Right a bit")
+            turn(2,0.5)
+        else:
+            print("Maybe there is a problem!")      # Flag for debug
+    else:   # if codeS is in listS then the robot will turn and the counter will increase its value
+        print("Already associated!")
+        turn(20,0.5)
+        cont=cont+1
+        
